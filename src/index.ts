5616f5c3d856c543d4ec86a4c5fa90d75602dad3
Gideon Idoko <iamgideonidoko@gmail.com>
2022-09-02 07:56:13 +0100

    Add 404 and global error handlers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createServer } from 'http';
-import express, { Application, Request, Response } from 'express';
+import express from 'express';
+import type { Application, Request, Response } from 'express';
 import { config } from 'dotenv';
 import { envConfig } from './config/environment.config';
 import xss from 'xss-clean';
@@ -11,6 +12,7 @@ import limiter from './config/limiter.config';
 import appCors from './config/cors.config';
 import getApolloServer from './graphql/index';
 import { prisma } from './graphql/context';
+import errorHandler from './middlewares/error.middleware';
 
 config();
 
@@ -18,7 +20,11 @@ config();
   const app: Application = express();
 
   // development logging
-  if (envConfig.isDevelopment) app.use(morgan('dev'));
+  if (envConfig.isProduction) {
+    app.use(morgan('common'));
+  } else {
+    app.use(morgan('dev'));
+  }
 
   // limit size of request payload
   app.use(express.json({ limit: '1MB' }));
@@ -60,7 +66,8 @@ config();
   // Routes
   app.get('/', (_req: Request, res: Response) => {
     res.status(200).json({
-      service: 'OgaRepair Core Backend',
+      status: 'success',
+      message: 'Service is up and running!',
     });
   });
 
@@ -68,6 +75,29 @@ config();
   const apolloServer = getApolloServer(httpServer);
   await apolloServer.start();
   apolloServer.applyMiddleware({ app, path: '/api/graphql' });
+
+  // Error for unhandled routes
+  app.use((_, res: Response) => {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Route not found!',
+    });
+  });
+
+  app.use(errorHandler);
+
+  // throw unhandled rejection to a fallback handler
+  process.on('unhandledRejection', (reason: Error) => {
+    console.log('\x1b[31m%s\x1b[0m', `Unhandled Rejection: ${reason}`);
+    throw reason;
+  });
+
+  // kill app if there's an uncaught exception
+  process.on('uncaughtException', (error: Error) => {
+    console.log('\x1b[31m%s\x1b[0m', `UncaughtException Error: ${error}`);
+    process.exit(1);
+  });
+
   await new Promise<void>((resolve) => httpServer.listen({ port: envConfig.port }, resolve));
   console.log(`🚀 HTTP Server ready at http://localhost:${envConfig.port}`);
   console.log(`💹🚀 GraphQL Server ready at http://localhost:${envConfig.port}${apolloServer.graphqlPath}`);

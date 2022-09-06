23165980efd5f31f12251fad73cf707ff32da979
Gideon Idoko <iamgideonidoko@gmail.com>
2022-09-06 04:22:52 +0100

    Make the data arg in logger methods optional

--- src/helpers/logger.helper.ts
+++ src/helpers/logger.helper.ts
@@ -1,22 +1,22 @@
 import { ILogger } from '../interfaces/helper.interface';
 
 const logger: ILogger = {
-  success(message: string, data: unknown) {
+  success(message: string, data?: unknown) {
     console.log('\x1b[32m%s\x1b[0m', `${message}: `, data);
   },
-  error(message: string, data: unknown) {
+  error(message: string, data?: unknown) {
     console.log('\x1b[31m%s\x1b[0m', `${message}: `, data);
   },
-  info(message: string, data: unknown) {
+  info(message: string, data?: unknown) {
     console.log('\x1b[34m%s\x1b[0m', `${message}: `, data);
   },
-  successBg(message: string, data: unknown) {
+  successBg(message: string, data?: unknown) {
     console.log('\x1b[42m%s\x1b[0m', `${message}: `, data);
   },
-  errorBg(message: string, data: unknown) {
+  errorBg(message: string, data?: unknown) {
     console.log('\x1b[41m%s\x1b[0m', `${message}: `, data);
   },
-  infoBg(message: string, data: unknown) {
+  infoBg(message: string, data?: unknown) {
     console.log('\x1b[44m%s\x1b[0m', `${message}: `, data);
   },
 };
